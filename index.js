ad4b0fbfa4f823914e3dfa84ac14dc659ae8c56f
Ms. Alina <alina@Alina.local>
2024-05-16 14:19:35 +0800

    add

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ let circ15 = document.getElementById("m15");
 let point15 = document.getElementsByClassName("m15");
 let drink = document.getElementById("drink");
 let health = document.getElementById("health");
+let bus = document.getElementById("bus");
 let point_drink = document.getElementsByClassName("drink");
 let point_health = document.getElementsByClassName("health");
+let point_bus = document.getElementsByClassName("bus");
 circ5.addEventListener("click",show_5,false);
 circ10.addEventListener("click",show_10,false);
 circ15.addEventListener("click",show_15,false);
 drink.addEventListener("click",show_d,false);
 health.addEventListener("click",show_h,false);
+bus.addEventListener("click",show_b,false);
 let show = [];
 let num = 15;
 
@@ -25,10 +28,14 @@ function show_5(){
         else if (i=="health"){
             show_h();
         }
+        else if (i=="bus"){
+            show_b();
+        }
     }
     if (show.length == 0){
         show_d();
         show_h();
+        show_b();
     }
     for (let i of point10){
         i.style.setProperty("visibility","hidden");
@@ -62,6 +69,9 @@ function hide_5(){
             else if (i=="health"){
                 show_h();
             }
+            else if (i=="bus"){
+                show_b();
+            }
         }
     }
     circ5.style.setProperty("background-color","white");
@@ -80,10 +90,14 @@ function show_10(){
         else if (i=="health"){
             show_h();
         }
+        else if (i=="bus"){
+            show_b();
+        }
     }
     if (show.length == 0){
         show_d();
         show_h();
+        show_b();
     }
     for (let i of point15){
         i.style.setProperty("visibility","hidden");
@@ -117,6 +131,9 @@ function hide_10(){
             else if (i=="health"){
                 show_h();
             }
+            else if (i=="bus"){
+                show_b();
+            }
         }
     }
     circ10.style.setProperty("background-color","white");
@@ -135,10 +152,14 @@ function show_15(){
         else if (i=="health"){
             show_h();
         }
+        else if (i=="bus"){
+            show_b();
+        }
     }
     if (show.length == 0){
         show_d();
         show_h();
+        show_b();
     }
     circ15.style.setProperty("background-color","rgb(243,243,243)");
     circ5.style.setProperty("background-color","white");
@@ -170,6 +191,9 @@ function hide_15(){
             }
             else if (i=="health"){
                 show_h();
+            }
+            else if (i=="bus"){
+                show_b();
             }
         }
     }
@@ -242,4 +266,36 @@ function hide_h(){
     health.removeEventListener("click",hide_h,false);
     health.addEventListener("click",show_h,false);
     show.splice(show.indexOf("health"),1)
+}
+
+function show_b(){
+    for (let i of point_bus){
+        i.style.setProperty("visibility","visible");
+    }
+    if (num<10){
+        for (let i of point10){
+            i.style.setProperty("visibility","hidden");
+        }
+    }
+    if (num<15){
+        for (let i of point15){
+            i.style.setProperty("visibility","hidden");
+        }
+    }
+    bus.style.setProperty("background-color","rgb(190,190,190)")
+    bus.removeEventListener("click",show_b,false);
+    bus.addEventListener("click",hide_b,false);
+    if (show.indexOf("bus")==-1){
+        show.push("bus");
+    }
+}
+
+function hide_b(){
+    for (let i of point_bus){
+        i.style.setProperty("visibility","hidden");
+    }
+    bus.style.setProperty("background-color","rgb(209,209,209)")
+    bus.removeEventListener("click",hide_b,false);
+    bus.addEventListener("click",show_b,false);
+    show.splice(show.indexOf("bus"),1)
 }
